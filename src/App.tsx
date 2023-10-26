d97a0d550f998967e9b888c630df48bbd6841b10
Reli <naitvenefy@gmail.com>
2023-10-26 17:59:49 -0600

    melodic minor scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "components/Header";
 import ChordsDisplay from "components/ChordsDisplay";
 import ChordsGenerator from "components/ChordsGenerator";
+import { Chord } from "./App.d";
 
 import "./App.css";
 
+const Scales = {
+  Major: Symbol(),
+  NaturalMinor: Symbol(),
+  MelodicMinor: Symbol(),
+};
+
 function App() {
+  const [selectedScale, setSelectedScale] = useState<Symbol | null>(null);
+  const [amountOfRandomChords, setAmountOfRandomChords] = useState<number>(0);
+  const [randomChords, setRandomChords] = useState<Chord[]>([]);
+
   return (
     <>
       <Header />
