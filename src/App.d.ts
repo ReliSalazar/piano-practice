eb29f36ad66fcd790f4089659838767b7f1a8687
Reli <naitvenefy@gmail.com>
2023-10-26 18:05:35 -0600

    state and interfaces to work with chords

--- src/App.d.ts
+++ src/App.d.ts
@@ -1,1 +1,25 @@
-export interface Chord {}
+export interface Anotation {
+  Title: string;
+  Body: string;
+}
+
+export interface BaseChord {
+  ScaleIntervals: string[];
+  ScaleSemiNotes: number[];
+  Name: string;
+  Image: string;
+  Anotations: Anotation[];
+}
+
+export interface NaturalChord extends BaseChord {
+  Notes: string[];
+  FingeringLH: number[];
+  FingeringRH: number[];
+}
+
+export interface MelodicChord extends BaseChord {
+  NotesAsc: string[];
+  NotesDesc: string[];
+}
+
+type Chord = NaturalChord | MelodicChord;
